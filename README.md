f3759b615fca73d0219d3ed94e4839d48c61e47b
Mitesh Upadhyay <upadhyaymitesh91@gmail.com>
2026-03-25 01:26:56 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Thunderstorm-Forcasting-with-MLFlow-Tracking
+uv init
+
+uv venv thenv
+
+thenv\Scripts\activate
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from thunderstorm-forcasting-with-mlflow-tracking!")
+
+
+if __name__ == "__main__":
+    main()
